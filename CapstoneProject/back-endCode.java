21fd934bfb756935f25fb84b38548be8f36045aa
raymondjoaquinaquino <raymondjoaquin.aquino@uap.asia>
2026-02-26 21:49:26 +0800

    Update back-endCode.java

--- CapstoneProject/back-endCode.java
+++ CapstoneProject/back-endCode.java
@@ -1,6 +1,34 @@
 //User Authentication
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public back-endCode
 {
+  private String emailAddress;
+  private String password;
+
+  public String getEmailAddress()
+  {
+    
+   return emailAddress; 
+    
+  }
   
+  public void setEmailAddress(String newEmailAddress)
+  {
+    this.newEmailAddress = emailAddress
+    
+  }
+
+  public String
+  
+  public static void main(String[] args)
+  {
+    
+
+
+
+    
+  }
 }
